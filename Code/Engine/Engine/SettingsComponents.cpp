fac0c2448b416b5263f5596f6610104fb6a066b3
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2024-09-26 22:07:07 -0400

    Updated SettingsComponent

--- Code/Engine/Engine/SettingsComponents.cpp
+++ Code/Engine/Engine/SettingsComponents.cpp
@@ -9,6 +9,13 @@ namespace
 	const str::StringView strAreHiddenEnabled = "m_AreHiddenEnabled";
 	const str::StringView strAreLinesEnabled = "m_AreLinesEnabled";
 	const str::StringView strArePhysicsEnabled = "m_ArePhysicsEnabled";
+	const str::StringView strAudio = "m_Audio";
+	const str::StringView strCamera = "m_Camera";
+	const str::StringView strEffectVolume = "m_EffectVolume";
+	const str::StringView strMusicVolume = "m_MusicVolume";
+	const str::StringView strRotateSpeed = "m_RotateSpeed";
+	const str::StringView strTranslateSpeed = "m_TranslateSpeed";
+	const str::StringView strZoomSpeed = "m_ZoomSpeed";
 }
 
 template<>
@@ -19,7 +26,6 @@ void eng::Visitor::ReadCustom(eng::settings::DebugComponent& value) const
 	Read(strAreLinesEnabled, value.m_AreLinesEnabled, value.m_AreLinesEnabled);
 	Read(strArePhysicsEnabled, value.m_ArePhysicsEnabled, value.m_ArePhysicsEnabled);
 }
-
 template<>
 void eng::Visitor::WriteCustom(const eng::settings::DebugComponent& value)
 {
@@ -27,4 +33,45 @@ void eng::Visitor::WriteCustom(const eng::settings::DebugComponent& value)
 	Write(strAreHiddenEnabled, value.m_AreHiddenEnabled);
 	Write(strAreLinesEnabled, value.m_AreLinesEnabled);
 	Write(strArePhysicsEnabled, value.m_ArePhysicsEnabled);
+}
+
+template<>
+void eng::Visitor::ReadCustom(eng::settings::LocalComponent& value) const
+{
+	Read(strAudio, value.m_Audio, value.m_Audio);
+	Read(strCamera, value.m_Camera, value.m_Camera);
+}
+template<>
+void eng::Visitor::WriteCustom(const eng::settings::LocalComponent& value)
+{
+	Write(strAudio, value.m_Audio);
+	Write(strCamera, value.m_Camera);
+}
+
+template<>
+void eng::Visitor::ReadCustom(eng::settings::Audio& value) const
+{
+	Read(strEffectVolume, value.m_EffectVolume, value.m_EffectVolume);
+	Read(strMusicVolume, value.m_MusicVolume, value.m_MusicVolume);
+}
+template<>
+void eng::Visitor::WriteCustom(const eng::settings::Audio& value)
+{
+	Write(strEffectVolume, value.m_EffectVolume);
+	Write(strMusicVolume, value.m_MusicVolume);
+}
+
+template<>
+void eng::Visitor::ReadCustom(eng::settings::Camera& value) const
+{
+	Read(strTranslateSpeed, value.m_TranslateSpeed, value.m_TranslateSpeed);
+	Read(strRotateSpeed, value.m_RotateSpeed, value.m_RotateSpeed);
+	Read(strZoomSpeed, value.m_ZoomSpeed, value.m_ZoomSpeed);
+}
+template<>
+void eng::Visitor::WriteCustom(const eng::settings::Camera& value)
+{
+	Write(strTranslateSpeed, value.m_TranslateSpeed);
+	Write(strRotateSpeed, value.m_RotateSpeed);
+	Write(strZoomSpeed, value.m_ZoomSpeed);
 }
--- Code/Engine/Engine/SettingsLocalSystem.cpp
+++ Code/Engine/Engine/SettingsLocalSystem.cpp
@@ -14,58 +14,6 @@
 namespace
 {
 	const str::StringView strFilename = "Settings.toml";
-
-	const str::StringView strAudio = "m_Audio";
-	const str::StringView strCamera = "m_Camera";
-	const str::StringView strEffectVolume = "m_EffectVolume";
-	const str::StringView strMusicVolume = "m_MusicVolume";
-	const str::StringView strTranslateSpeed = "m_TranslateSpeed";
-	const str::StringView strRotateSpeed = "m_RotateSpeed";
-	const str::StringView strZoomSpeed = "m_ZoomSpeed";
-}
-
-template<>
-void eng::Visitor::ReadCustom(eng::settings::LocalComponent& value) const
-{
-	Read(strAudio, value.m_Audio, value.m_Audio);
-	Read(strCamera, value.m_Camera, value.m_Camera);
-}
-
-template<>
-void eng::Visitor::ReadCustom(eng::settings::Audio& value) const
-{
-	Read(strEffectVolume, value.m_EffectVolume, value.m_EffectVolume);
-	Read(strMusicVolume, value.m_MusicVolume, value.m_MusicVolume);
-}
-
-template<>
-void eng::Visitor::ReadCustom(eng::settings::Camera& value) const
-{
-	Read(strTranslateSpeed, value.m_TranslateSpeed, value.m_TranslateSpeed);
-	Read(strRotateSpeed, value.m_RotateSpeed, value.m_RotateSpeed);
-	Read(strZoomSpeed, value.m_ZoomSpeed, value.m_ZoomSpeed);
-}
-
-template<>
-void eng::Visitor::WriteCustom(const eng::settings::LocalComponent& value)
-{
-	Write(strAudio, value.m_Audio);
-	Write(strCamera, value.m_Camera);
-}
-
-template<>
-void eng::Visitor::WriteCustom(const eng::settings::Audio& value)
-{
-	Write(strEffectVolume, value.m_EffectVolume);
-	Write(strMusicVolume, value.m_MusicVolume);
-}
-
-template<>
-void eng::Visitor::WriteCustom(const eng::settings::Camera& value)
-{
-	Write(strTranslateSpeed, value.m_TranslateSpeed);
-	Write(strRotateSpeed, value.m_RotateSpeed);
-	Write(strZoomSpeed, value.m_ZoomSpeed);
 }
 
 void eng::settings::LocalSystem::Initialise(World& world)
