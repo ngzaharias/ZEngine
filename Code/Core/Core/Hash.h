85d6dbe4e9b9c6865ef146d00ed5494ef7ba5d93
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2024-10-03 20:41:15 -0400

    Updated hash
    Replaced hash with own implementation rather than a 3rdParty one.

--- Code/Core/Core/Hash.h
+++ Code/Core/Core/Hash.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "Core/String.h"
+#include "Core/StringView.h"
 #include "Core/Types.h"
 
 namespace str
 {
 	using Hash = uint32;
 
-	str::Hash ToHash(const char* value);
-	str::Hash ToHash(const str::String& value);
+	str::Hash ToHash(const str::StringView& value);
 }
