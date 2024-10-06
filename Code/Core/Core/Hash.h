d528bcbc053b68c854da678129dad8cd5b27a7d0
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2024-10-06 19:26:53 -0400

    Update Hash.h

--- Code/Core/Core/Hash.h
+++ Code/Core/Core/Hash.h
@@ -11,8 +11,7 @@ namespace str
 	using Hash32 = uint32;
 	using Hash64 = uint64;
 
-	// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
-	constexpr str::Hash32 ToHash(const str::StringView& value)
+	constexpr str::Hash32 ToHash32(const str::StringView& value)
 	{
 		constexpr uint32 offset = 2166136261;
 		constexpr uint32 prime = 16777619;
@@ -23,7 +22,7 @@ namespace str
 		return hash;
 	}
 
-	constexpr str::Hash64 ToHashU64(const str::StringView& value)
+	constexpr str::Hash64 ToHash64(const str::StringView& value)
 	{
 		constexpr uint64 offset = 14695981039346656037ull;
 		constexpr uint64 prime = 1099511628211ull;
@@ -32,5 +31,11 @@ namespace str
 		for (const char v : value)
 			hash = (hash ^ static_cast<uint32>(v)) * prime;
 		return hash;
+	}
+
+	// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+	constexpr str::Hash32 ToHash(const str::StringView& value)
+	{
+		return ToHash32(value);
 	}
 }
