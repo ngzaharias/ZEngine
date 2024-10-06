bd264989e0909df49508c39de2aec656cfa016d1
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2024-10-05 21:19:34 -0400

    Updated Hash to be constexpr

--- Code/Core/Core/Hash.h
+++ Code/Core/Core/Hash.h
@@ -1,12 +1,36 @@
 #pragma once
 
-#include "Core/String.h"
 #include "Core/StringView.h"
-#include "Core/Types.h"
 
 namespace str
 {
-	using Hash = uint32;
+	using uint32 = uint32_t;
+	using uint64 = uint64_t;
 
-	str::Hash ToHash(const str::StringView& value);
+	using Hash = uint32;
+	using Hash32 = uint32;
+	using Hash64 = uint64;
+
+	// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+	constexpr str::Hash32 ToHash(const str::StringView& value)
+	{
+		constexpr uint32 offset = 2166136261;
+		constexpr uint32 prime = 16777619;
+
+		uint32 hash = offset;
+		for (const char v : value)
+			hash = (hash ^ static_cast<uint32>(v)) * prime;
+		return hash;
+	}
+
+	constexpr str::Hash64 ToHashU64(const str::StringView& value)
+	{
+		constexpr uint64 offset = 14695981039346656037ull;
+		constexpr uint64 prime = 1099511628211ull;
+
+		uint64 hash = offset;
+		for (const char v : value)
+			hash = (hash ^ static_cast<uint32>(v)) * prime;
+		return hash;
+	}
 }
