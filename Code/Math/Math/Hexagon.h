498a8b8312a78248071537ceef0bff5cdcf880d1
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2024-11-12 21:33:47 -0500

    Update Hexagon.h

--- Code/Math/Math/Hexagon.h
+++ Code/Math/Math/Hexagon.h
@@ -4,11 +4,17 @@ namespace hexagon
 {
 	struct Axial
 	{
+		operator Vector2i() { return Vector2i(q, r); }
+		operator Vector2i() const { return Vector2i(q, r); }
+
 		int32 q, r;
 	};
 
 	struct Offset
 	{
+		operator Vector2i() { return Vector2i(col, row); }
+		operator Vector2i() const { return Vector2i(col, row); }
+
 		int32 col, row;
 	};
 }
