82babfb58eb7bfaaf2c2f22895629d036e6d432c
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-03-09 11:00:00 -0400

    Update Window.h

--- Code/Engine/Engine/GLFW/Window.h
+++ Code/Engine/Engine/GLFW/Window.h
@@ -28,11 +28,9 @@ namespace glfw
 		void PreUpdate(const GameTime& gameTime) override;
 		void PostUpdate(const GameTime& gameTime) override;
 
-		Vector2u GetResolution() const override { return m_Config.m_Resolution; }
 		GLFWwindow* GetWindow() const { return m_Window; }
 
 		bool HasResized() const override { return m_HasResized; }
-		void SetResized(bool value) { m_HasResized = value; }
 		bool ShouldClose() const override;
 
 		void GatherKeyboard(Set<input::EKeyboard>& out_Keys) const;
