27312436b18d9c0001f11793c3dd14d06ca7b291
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-03-18 22:42:45 -0400

    Removed Transformer

--- Code/Imgui/imgui/Transformer.cpp
+++
@@ -1,9 +0,0 @@
-#include "imgui/Transformer.h"
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_internal.h"
-
-bool imgui::Translate2D(Matrix4x4& model, const Matrix4x4& view, const Matrix4x4& projection)
-{
-	return false;
-}
--- Code/Imgui/imgui/Transformer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class Matrix4x4;
-
-namespace imgui
-{
-	bool Translate2D(Matrix4x4& model, const Matrix4x4& view, const Matrix4x4& projection);
-}
