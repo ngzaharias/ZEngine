7f8438b5a134ab513fc89b0b109068bf788fa978
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-05-21 22:09:40 -0400

    Update SettingsComponents.cpp

--- Code/Engine/Engine/SettingsComponents.cpp
+++ Code/Engine/Engine/SettingsComponents.cpp
@@ -62,6 +62,7 @@ bool imgui::Inspector::WriteCustom(eng::settings::DebugComponent& value)
 	result |= Write("m_AreLinesEnabled", value.m_AreLinesEnabled);
 	result |= Write("m_ArePhysicsEnabled", value.m_ArePhysicsEnabled);
 	result |= Write("m_AreSoftbodyEnabled", value.m_AreSoftbodyEnabled);
+	result |= Write("m_IsEditorModeEnabled", value.m_IsEditorModeEnabled);
 	return result;
 }
 
