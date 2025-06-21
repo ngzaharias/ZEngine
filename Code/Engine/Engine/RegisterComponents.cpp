b6541a9d7fb0436b3463d0fcdb016ec1d1e264ef
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-06-21 12:49:21 -0400

    Update RegisterComponents.cpp

--- Code/Engine/Engine/RegisterComponents.cpp
+++ Code/Engine/Engine/RegisterComponents.cpp
@@ -34,92 +34,6 @@
 
 #include <yojimbo/yojimbo.h>
 
-namespace
-{
-	template<typename TComponent>
-	struct Default final
-	{
-		static void Read(TComponent& component, const MemBuffer& buffer) { }
-		static void Write(const TComponent& component, MemBuffer& buffer) { }
-	};
-
-	struct NameSerializer final
-	{
-		static void Read(ecs::NameComponent& component, const MemBuffer& buffer)
-		{
-			buffer.Read(component.m_Name);
-		}
-
-		static void Write(const ecs::NameComponent& component, MemBuffer& buffer)
-		{
-			buffer.Write(component.m_Name);
-		}
-	};
-
-	struct SpriteSerializer final
-	{
-		static void Read(eng::SpriteComponent& component, const MemBuffer& buffer)
-		{
-			buffer.Read(component.m_Size.x);
-			buffer.Read(component.m_Size.y);
-			buffer.Read(component.m_Sprite.m_Data);
-		}
-
-		static void Write(const eng::SpriteComponent& component, MemBuffer& buffer)
-		{
-			buffer.Write(component.m_Size.x);
-			buffer.Write(component.m_Size.y);
-			buffer.Write(component.m_Sprite.m_Data);
-		}
-	};
-
-	struct TransformSerializer final
-	{
-		static void Read(eng::TransformComponent& component, const MemBuffer& buffer)
-		{
-			buffer.Read(component.m_Translate.x);
-			buffer.Read(component.m_Translate.y);
-			buffer.Read(component.m_Translate.z);
-
-			buffer.Read(component.m_Rotate.m_Pitch);
-			buffer.Read(component.m_Rotate.m_Yaw);
-			buffer.Read(component.m_Rotate.m_Roll);
-
-			buffer.Read(component.m_Scale.x);
-			buffer.Read(component.m_Scale.y);
-			buffer.Read(component.m_Scale.z);
-		}
-
-		static void Write(const eng::TransformComponent& component, MemBuffer& buffer)
-		{
-			buffer.Write(component.m_Translate.x);
-			buffer.Write(component.m_Translate.y);
-			buffer.Write(component.m_Translate.z);
-
-			buffer.Write(component.m_Rotate.m_Pitch);
-			buffer.Write(component.m_Rotate.m_Yaw);
-			buffer.Write(component.m_Rotate.m_Roll);
-
-			buffer.Write(component.m_Scale.x);
-			buffer.Write(component.m_Scale.y);
-			buffer.Write(component.m_Scale.z);
-		}
-	};
-
-	struct UserSerializer final
-	{
-		static void Read(net::UserComponent& component, const MemBuffer& buffer)
-		{
-			buffer.Read(component.m_UserId.m_Value);
-		}
-
-		static void Write(const net::UserComponent& component, MemBuffer& buffer)
-		{
-			buffer.Write(component.m_UserId.m_Value);
-		}
-	};
-}
-
 void eng::RegisterClientComponents(ecs::EntityWorld& entityWorld)
 {
 	entityWorld.RegisterComponent<eng::application::CloseRequestComponent>();
@@ -194,9 +108,9 @@ void eng::RegisterSharedComponents(ecs::EntityWorld& entityWorld, net::Component
 	entityWorld.RegisterSingleton<eng::VersionComponent>();
 
 	// serialization
-	serializer.RegisterComponent<eng::camera::ProjectionComponent, Default<eng::camera::ProjectionComponent>>();
-	serializer.RegisterComponent<ecs::NameComponent, NameSerializer>();
-	serializer.RegisterComponent<eng::SpriteComponent, SpriteSerializer>();
-	serializer.RegisterComponent<eng::TransformComponent, TransformSerializer>();
-	serializer.RegisterComponent<net::UserComponent, UserSerializer>();
+	//serializer.RegisterComponent<eng::camera::ProjectionComponent, Default<eng::camera::ProjectionComponent>>();
+	//serializer.RegisterComponent<ecs::NameComponent, NameSerializer>();
+	//serializer.RegisterComponent<eng::SpriteComponent, SpriteSerializer>();
+	//serializer.RegisterComponent<eng::TransformComponent, TransformSerializer>();
+	//serializer.RegisterComponent<net::UserComponent, UserSerializer>();
 }
