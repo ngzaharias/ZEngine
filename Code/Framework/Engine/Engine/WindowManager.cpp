6e04aa0053f95c5439c8f89c8497952741be8650
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-08-07 21:26:49 -0400

    Update WindowManager.cpp

--- Code/Framework/Engine/Engine/WindowManager.cpp
+++ Code/Framework/Engine/Engine/WindowManager.cpp
@@ -48,6 +48,7 @@ void eng::WindowManager::Initialise()
 	if (auto* window = reinterpret_cast<glfw::Window*>(m_Windows[0]))
 	{
 		auto* glfwWindow = window->GetWindow();
+		glfwSetInputMode(glfwWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 		glfwSetWindowTitle(glfwWindow, "With My Little Eye");
 		glfwMakeContextCurrent(glfwWindow);
 
