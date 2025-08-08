330ed14bc31ae82b3fe3e22b1d641362e3bade77
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-08-07 22:45:17 -0400

    Update CursorSystem.cpp

--- Code/Framework/Engine/Engine/CursorSystem.cpp
+++ Code/Framework/Engine/Engine/CursorSystem.cpp
@@ -5,7 +5,7 @@
 #include "ECS/QueryTypes.h"
 #include "ECS/WorldView.h"
 #include "Engine/CameraComponent.h"
-#include "Engine/CursorComponent.h"
+//#include "Engine/CursorComponent.h"
 #include "Engine/InputManager.h"
 #include "Engine/SpriteComponent.h"
 #include "Engine/TransformComponent.h"
@@ -14,12 +14,12 @@ void eng::CursorSystem::Update(World& world, const GameTime& gameTime)
 {
 	PROFILE_FUNCTION();
 
-	const auto& inputManager = world.ReadResource<eng::InputManager>();
+	//const auto& inputManager = world.ReadResource<eng::InputManager>();
 
-	for (const ecs::Entity& entity : world.Query<ecs::query::Include<eng::TransformComponent, const eng::CursorComponent>>())
-	{
-		auto& transformComponent = world.AddComponent<eng::TransformComponent>(entity);
-		transformComponent.m_Translate;
-		transformComponent.m_Rotate;
-	}
+	//for (const ecs::Entity& entity : world.Query<ecs::query::Include<eng::TransformComponent, const eng::CursorComponent>>())
+	//{
+	//	auto& transformComponent = world.AddComponent<eng::TransformComponent>(entity);
+	//	transformComponent.m_Translate;
+	//	transformComponent.m_Rotate;
+	//}
 }
