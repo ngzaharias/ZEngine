897ef1d6b74796b95e3af415eb7ae92371f949fb
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-12-21 21:57:11 -0500

    Update TypeList.h
    - added support to merge typelists

--- Code/Framework/Core/Core/TypeList.h
+++ Code/Framework/Core/Core/TypeList.h
@@ -7,4 +7,14 @@ struct TypeList
 	using Append = TypeList<Types..., Added...>;
 
 	using NonConst = TypeList<std::remove_const_t<Types>...>;
+
+};
+
+template<typename...>
+struct TypeMerge;
+
+template<typename ...A, typename ...B>
+struct TypeMerge<TypeList<A...>, TypeList<B...>>
+{
+	using TypeList = TypeList<A..., B...>;
 };
