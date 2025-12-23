3ca9b5da25c11e67424ba7a61cc739351ab96ac2
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2025-12-22 20:26:23 -0500

    Update TypeList.h
    - added type method to build a typelist of only the const members

--- Code/Framework/Core/Core/TypeList.h
+++ Code/Framework/Core/Core/TypeList.h
@@ -7,7 +7,6 @@ struct TypeList
 	using Append = TypeList<Types..., Added...>;
 
 	using NonConst = TypeList<std::remove_const_t<Types>...>;
-
 };
 
 template<typename...>
@@ -17,4 +16,26 @@ template<typename ...A, typename ...B>
 struct TypeMerge<TypeList<A...>, TypeList<B...>>
 {
 	using TypeList = TypeList<A..., B...>;
+};
+
+template <typename List>
+struct TypeConst;
+
+template <>
+struct TypeConst<TypeList<>>
+{
+	using type = TypeList<>;
+};
+
+template <typename Head, typename... Tail>
+struct TypeConst<TypeList<Head, Tail...>>
+{
+private:
+	using List = typename TypeConst<TypeList<Tail...>>::type;
+
+public:
+	using type = std::conditional_t<
+		std::is_const_v<Head>, 
+		typename TypeMerge<TypeList<Head>, List>::TypeList,
+		List>;
 };
