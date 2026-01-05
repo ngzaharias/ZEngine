6f257b3de17e720bef9f1d0217fc8e372590f3aa
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2026-01-04 20:53:46 -0500

    Updated to work without a GameServer and instead just P2P

--- Code/Application/GameDebug/GameDebug/NetworkSystem.cpp
+++ Code/Application/GameDebug/GameDebug/NetworkSystem.cpp
@@ -19,7 +19,7 @@
 
 namespace
 {
-	bool SendServerData(HSteamNetConnection connection, const void* pData, uint32 nSizeOfData)
+	bool SendMessage(HSteamNetConnection connection, const void* pData, uint32 nSizeOfData)
 	{
 		int nSendFlags = k_nSteamNetworkingSend_Unreliable;
 		EResult res = SteamNetworkingSockets()->SendMessageToConnection(connection, pData, nSizeOfData, nSendFlags, nullptr);
@@ -51,11 +51,6 @@ namespace
 	}
 }
 
-debug::NetworkSystem::NetworkSystem()
-	: m_CallbackLobbyDataUpdated(this, &NetworkSystem::OnLobbyDataUpdatedCallback)
-{
-}
-
 void debug::NetworkSystem::Initialise(World& world)
 {
 	world.AddEvent<debug::NetworkWindowRequest>();
@@ -65,10 +60,10 @@ void debug::NetworkSystem::Update(World& world, const GameTime& gameTime)
 {
 	PROFILE_FUNCTION();
 
-	if (SteamNetworkingSockets() && m_Connection != k_HSteamNetConnection_Invalid)
+	if (m_Server != k_HSteamNetConnection_Invalid)
 	{
 		SteamNetworkingMessage_t* msgs[32];
-		int res = SteamNetworkingSockets()->ReceiveMessagesOnConnection(m_Connection, msgs, 32);
+		int res = SteamNetworkingSockets()->ReceiveMessagesOnConnection(m_Server, msgs, 32);
 		for (int i = 0; i < res; i++)
 		{
 			SteamNetworkingMessage_t* message = msgs[i];
@@ -82,7 +77,8 @@ void debug::NetworkSystem::Update(World& world, const GameTime& gameTime)
 	if (m_NetPollGroup)
 	{
 		SteamNetworkingMessage_t* msgs[128];
-		int numMessages = SteamGameServerNetworkingSockets()->ReceiveMessagesOnPollGroup(m_NetPollGroup, msgs, 128);
+		//int numMessages = SteamGameServerNetworkingSockets()->ReceiveMessagesOnPollGroup(m_NetPollGroup, msgs, 128);
+		int numMessages = SteamNetworkingSockets()->ReceiveMessagesOnPollGroup(m_NetPollGroup, msgs, 128);
 		for (int i = 0; i < numMessages; i++)
 		{
 			SteamNetworkingMessage_t* message = msgs[i];
@@ -122,95 +118,55 @@ void debug::NetworkSystem::Update(World& world, const GameTime& gameTime)
 				addr.Clear();
 				addr.m_port = 27020;
 
-				uint32 unIP = 0;
-				uint16 gamePort = 27015;
-				uint16 usQueryPort = 27016;
-				EServerMode eMode = eServerModeNoAuthentication;
-				const char* version = "alpha-0.0.1";
-
-				SteamErrMsg errMsg = { 0 };
-				if (SteamGameServer_InitEx(unIP, gamePort, usQueryPort, eMode, version, &errMsg) == k_ESteamAPIInitResult_OK)
-				{
-					Z_LOG(ELog::Network, "Server Started");
-
-					SteamGameServer()->LogOnAnonymous();
-					//m_ListenSocket = SteamGameServerNetworkingSockets()->CreateListenSocketIP(addr, 0, nullptr);
-					m_ListenSocket = SteamGameServerNetworkingSockets()->CreateListenSocketP2P(0, 0, nullptr);
-					m_NetPollGroup = SteamGameServerNetworkingSockets()->CreatePollGroup();
-				}
+				Z_LOG(ELog::Network, "Server Started");
+				m_ListenSocketId = SteamNetworkingSockets()->CreateListenSocketP2P(0, 0, nullptr);
+				m_ListenSocketIp = SteamNetworkingSockets()->CreateListenSocketIP(addr, 0, nullptr);
+				m_NetPollGroup = SteamNetworkingSockets()->CreatePollGroup();
 			}
 
 			if (ImGui::Button("stop server"))
 			{
 				Z_LOG(ELog::Network, "Server Stopped");
-				SteamNetworkingSockets()->CloseListenSocket(m_ListenSocket);
+				SteamNetworkingSockets()->CloseListenSocket(m_ListenSocketId);
+				SteamNetworkingSockets()->CloseListenSocket(m_ListenSocketIp);
 				SteamNetworkingSockets()->DestroyPollGroup(m_NetPollGroup);
-				SteamGameServer()->LogOff();
-				SteamGameServer_Shutdown();
 
+				m_ListenSocketId = k_HSteamListenSocket_Invalid;
+				m_ListenSocketIp = k_HSteamListenSocket_Invalid;
 				m_NetPollGroup = k_HSteamNetPollGroup_Invalid;
-				m_ListenSocket = k_HSteamListenSocket_Invalid;
 			}
 
-			if (ImGui::Button("join server"))
+			if (ImGui::Button("join server (id)"))
 			{
-				if (false)
-				{
-					SteamNetworkingIPAddr addr{};
-					addr.ParseString("127.0.0.1:27020");
-					m_Connection = SteamNetworkingSockets()->ConnectByIPAddress(addr, 0, nullptr);
-				}
-				else
-				{
-					CSteamID serverId = SteamGameServer()->GetSteamID();
-					SteamNetworkingIdentity identity;
-					identity.SetSteamID(serverId);
-					m_Connection = SteamNetworkingSockets()->ConnectP2P(identity, 0, 0, nullptr);
-				}
-
-				if (m_Connection == k_HSteamNetConnection_Invalid)
-					Z_LOG(ELog::Network, "Client failed to Connect");
+				CSteamID hostId = SteamUser()->GetSteamID();
+				SteamNetworkingIdentity identity;
+				identity.SetSteamID(hostId);
+				m_Server = SteamNetworkingSockets()->ConnectP2P(identity, 0, 0, nullptr);
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("(ip)"))
+			{
+				SteamNetworkingIPAddr addr{};
+				addr.ParseString("127.0.0.1:27020");
+				m_Server = SteamNetworkingSockets()->ConnectByIPAddress(addr, 0, nullptr);
 			}
 
 			if (ImGui::Button("leave server"))
 			{
-				SteamNetworkingSockets()->CloseConnection(m_Connection, 0, nullptr, false);
-				m_Connection = k_HSteamNetConnection_Invalid;
+				SteamNetworkingSockets()->CloseConnection(m_Server, 0, nullptr, false);
+				m_Server = k_HSteamNetConnection_Invalid;
 			}
 
-			if (ImGui::Button("message to server"))
+			if (ImGui::Button("message to client(s)"))
 			{
-				SendServerData(m_Connection, nullptr, 0);
+				for (const HSteamNetConnection client : m_Clients)
+					SendMessage(client, nullptr, 0);
 			}
-
-			ImGui::Separator();
-			ImGui::Separator();
-			ImGui::Separator();
-
-			if (ImGui::Button("start lobby"))
+			ImGui::SameLine();
+			if (ImGui::Button("server"))
 			{
-				Z_LOG(ELog::Network, "Lobby Started");
-				SteamAPICall_t hSteamAPICall = SteamMatchmaking()->CreateLobby(k_ELobbyTypePublic, 4);
-				m_SteamCallResultLobbyCreated.Set(hSteamAPICall, this, &NetworkSystem::OnLobbyCreated);
+				SendMessage(m_Server, nullptr, 0);
 			}
-
-			if (ImGui::Button("find lobby"))
-			{
-				SteamAPICall_t hSteamAPICall = SteamMatchmaking()->RequestLobbyList();
-				m_SteamCallResultLobbyMatchList.Set(hSteamAPICall, this, &debug::NetworkSystem::OnLobbyMatchListCallback);
-			}
-
-			if (ImGui::CollapsingHeader("join lobby"))
-			{
-				ImGui::Indent();
-				for (const NetworkLobby& lobby : m_Lobbies)
-				{
-					if (ImGui::Button(lobby.m_Name.c_str()))
-						SteamMatchmaking()->JoinLobby(lobby.m_SteamId);
-				}
-				ImGui::Unindent();
-			}
-
 		}
 		ImGui::End();
 
@@ -221,81 +177,36 @@ void debug::NetworkSystem::Update(World& world, const GameTime& gameTime)
 
 void debug::NetworkSystem::OnNetConnectionStatusChanged(SteamNetConnectionStatusChangedCallback_t* pCallback)
 {
-	HSteamNetConnection hConn = pCallback->m_hConn;
-	SteamNetConnectionInfo_t info = pCallback->m_info;
-	ESteamNetworkingConnectionState eOldState = pCallback->m_eOldState;
-	if (info.m_hListenSocket && eOldState == k_ESteamNetworkingConnectionState_None && info.m_eState == k_ESteamNetworkingConnectionState_Connecting)
+	const HSteamNetConnection connection = pCallback->m_hConn;
+	const SteamNetConnectionInfo_t info = pCallback->m_info;
+	const ESteamNetworkingConnectionState eOldState = pCallback->m_eOldState;
+	const ESteamNetworkingConnectionState eNewState = info.m_eState;
+
+	const bool isConnecting =
+		eOldState != k_ESteamNetworkingConnectionState_Connecting &&
+		eNewState == k_ESteamNetworkingConnectionState_Connecting;
+	if (info.m_hListenSocket && isConnecting)
 	{
-		EResult res = SteamGameServerNetworkingSockets()->AcceptConnection(hConn);
+		const EResult res = SteamNetworkingSockets()->AcceptConnection(connection);
 		if (res != k_EResultOK)
 		{
 			Z_LOG(ELog::Network, "AcceptConnection returned {}", static_cast<int32>(res));
-			SteamGameServerNetworkingSockets()->CloseConnection(hConn, k_ESteamNetConnectionEnd_AppException_Generic, "Failed to accept connection", false);
+			SteamNetworkingSockets()->CloseConnection(connection, k_ESteamNetConnectionEnd_AppException_Generic, "Failed to accept connection", false);
 			return;
 		}
 
+		Z_LOG(ELog::Network, "Client Accepted");
+		SteamNetworkingSockets()->SetConnectionPollGroup(connection, m_NetPollGroup);
+		SteamNetworkingSockets()->SendMessageToConnection(connection, nullptr, 0, k_nSteamNetworkingSend_Reliable, nullptr);
+		m_Clients.Append(connection);
+	}
+
+	const bool hasConnected =
+		eOldState != k_ESteamNetworkingConnectionState_Connected &&
+		eNewState == k_ESteamNetworkingConnectionState_Connected;
+	if (!info.m_hListenSocket && hasConnected)
+	{
 		Z_LOG(ELog::Network, "Client Connected");
-		SteamGameServerNetworkingSockets()->SetConnectionPollGroup(hConn, m_NetPollGroup);
-		SteamGameServerNetworkingSockets()->SendMessageToConnection(hConn, nullptr, 0, k_nSteamNetworkingSend_Reliable, nullptr);
+		SteamNetworkingSockets()->SendMessageToConnection(connection, nullptr, 0, k_nSteamNetworkingSend_Reliable, nullptr);
 	}
-}
-
-void debug::NetworkSystem::OnSteamServersConnected(SteamServersConnected_t* pCallback)
-{
-	SteamGameServer()->SetPasswordProtected(false);
-	SteamGameServer()->SetServerName("Koala's Game");
-	SteamGameServer()->SetMapName("TestLevel");
-	SteamGameServer()->SetMaxPlayerCount(4);
-}
-
-void debug::NetworkSystem::OnLobbyCreated(LobbyCreated_t* pCallback, bool bIOFailure)
-{
-	if (pCallback->m_eResult == k_EResultOK)
-	{
-		m_LobbyId = pCallback->m_ulSteamIDLobby;
-		SteamMatchmaking()->SetLobbyData(m_LobbyId, "name", "My Lobby");
-	}
-}
-
-void debug::NetworkSystem::OnLobbyMatchListCallback(LobbyMatchList_t* pCallback, bool bIOFailure)
-{
-	m_Lobbies.RemoveAll();
-
-	// lobbies are returned in order of closeness to the user, so add them to the list in that order
-	for (uint32 iLobby = 0; iLobby < pCallback->m_nLobbiesMatching; iLobby++)
-	{
-		NetworkLobby& lobby = m_Lobbies.Emplace();
-		lobby.m_SteamId = SteamMatchmaking()->GetLobbyByIndex(iLobby);
-		lobby.m_Name = SteamMatchmaking()->GetLobbyData(lobby.m_SteamId, "name");
-
-		if (lobby.m_Name.empty())
-			SteamMatchmaking()->RequestLobbyData(lobby.m_SteamId);
-	}
-}
-
-void debug::NetworkSystem::OnLobbyDataUpdatedCallback(LobbyDataUpdate_t* pCallback)
-{
-	for (NetworkLobby& lobby : m_Lobbies)
-	{
-		if (pCallback->m_ulSteamIDLobby == lobby.m_SteamId)
-		{
-			lobby.m_Name = SteamMatchmaking()->GetLobbyData(lobby.m_SteamId, "name");
-			break;
-		}
-	}
-}
-
-void debug::NetworkSystem::OnLobbyEntered(LobbyEnter_t* cb)
-{
-	if (cb->m_EChatRoomEnterResponse != k_EChatRoomEnterResponseSuccess)
-	{
-		Z_LOG(ELog::Network, "Failed to join lobby.");
-		return;
-	}
-
-	const CSteamID lobbyId = cb->m_ulSteamIDLobby;
-	const CSteamID hostId = SteamMatchmaking()->GetLobbyOwner(lobbyId);
-
-	Z_LOG(ELog::Network, "Joined lobby {}.", lobbyId.ConvertToUint64());
-	Z_LOG(ELog::Network, "Lobby host {}.", hostId.ConvertToUint64());
 }
--- Code/Application/GameDebug/GameDebug/NetworkSystem.h
+++ Code/Application/GameDebug/GameDebug/NetworkSystem.h
@@ -26,12 +26,6 @@ namespace debug
 	struct NetworkWindowComponent;
 	struct NetworkWindowRequest;
 
-	struct NetworkLobby
-	{
-		CSteamID m_SteamId = {};
-		str::String m_Name = {};
-	};
-
 	class NetworkSystem final : public ecs::System
 	{
 	public:
@@ -43,31 +37,21 @@ namespace debug
 			::Read<
 			debug::NetworkWindowRequest>;
 
-		NetworkSystem();
-
 		void Initialise(World& world);
 		
 		void Update(World& world, const GameTime& gameTime);
 
 	private:
-		STEAM_GAMESERVER_CALLBACK(NetworkSystem, OnNetConnectionStatusChanged, SteamNetConnectionStatusChangedCallback_t);
-		STEAM_GAMESERVER_CALLBACK(NetworkSystem, OnSteamServersConnected, SteamServersConnected_t);
-
-		void OnLobbyCreated(LobbyCreated_t* pCallback, bool bIOFailure);
-		CCallResult<NetworkSystem, LobbyCreated_t> m_SteamCallResultLobbyCreated;
-
-		CCallResult<NetworkSystem, LobbyMatchList_t> m_SteamCallResultLobbyMatchList;
-		void OnLobbyMatchListCallback(LobbyMatchList_t* pLobbyMatchList, bool bIOFailure);
-		STEAM_CALLBACK(NetworkSystem, OnLobbyDataUpdatedCallback, LobbyDataUpdate_t, m_CallbackLobbyDataUpdated);
-		STEAM_CALLBACK(NetworkSystem, OnLobbyEntered, LobbyEnter_t);
+		STEAM_CALLBACK(NetworkSystem, OnNetConnectionStatusChanged, SteamNetConnectionStatusChangedCallback_t);
 
 	private:
-		CSteamID m_LobbyId = {};
-		HSteamNetConnection m_Connection = k_HSteamNetConnection_Invalid;
-		HSteamListenSocket m_ListenSocket = k_HSteamListenSocket_Invalid;
-		HSteamNetPollGroup m_NetPollGroup = k_HSteamNetPollGroup_Invalid;
-
 		imgui::Identifier m_WindowIds = {};
-		Array<NetworkLobby> m_Lobbies = {};
+
+		HSteamNetConnection m_Server = k_HSteamNetConnection_Invalid;
+		Array<HSteamNetConnection> m_Clients = {};
+
+		HSteamListenSocket m_ListenSocketId = k_HSteamListenSocket_Invalid;
+		HSteamListenSocket m_ListenSocketIp = k_HSteamListenSocket_Invalid;
+		HSteamNetPollGroup m_NetPollGroup = k_HSteamNetPollGroup_Invalid;
 	};
 }
