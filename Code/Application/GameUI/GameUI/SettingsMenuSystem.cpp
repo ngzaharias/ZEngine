f4880f4b2eda05693c3a2741ff370f6c42bd04f8
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2026-04-11 21:51:00 -0400

    Updated SettingsMenuSystem

--- Code/Application/GameUI/GameUI/SettingsMenuSystem.cpp
+++ Code/Application/GameUI/GameUI/SettingsMenuSystem.cpp
@@ -29,12 +29,10 @@ void gui::settings_menu::MenuSystem::Update(World& world, const GameTime& gameTi
 
 	for (auto&& view : world.Query<ecs::query::Added<gui::settings_menu::WindowComponent>>())
 	{
-		gui::DCSettingsMenu::World other = world;
-
 		auto& uiManager = world.WriteResource<eng::UIManager>();
 		uiManager.CreateWidget(strSettingsMenu_xaml, true);
 		auto& dataContext = uiManager.WriteDataContext<gui::DCSettingsMenu>(strSettingsMenu_xaml);
-		dataContext.Initialise(other);
+		dataContext.Initialise(world);
 	}
 
 	for (auto&& view : world.Query<ecs::query::Removed<gui::settings_menu::WindowComponent>>())
