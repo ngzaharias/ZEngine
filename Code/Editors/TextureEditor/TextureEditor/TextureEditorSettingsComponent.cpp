c7aa46fc6c969bd1067dd8d35e6bf4c6ea4afa7f
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2026-05-25 21:05:57 -0400

    Update TextureEditorSettingsComponent.cpp

--- Code/Editors/TextureEditor/TextureEditor/TextureEditorSettingsComponent.cpp
+++ Code/Editors/TextureEditor/TextureEditor/TextureEditorSettingsComponent.cpp
@@ -7,7 +7,7 @@
 
 namespace
 {
-	const str::StringView strExtract = "m_Import";
+	const str::StringView strImport = "m_Import";
 	const str::StringView strOpen = "m_Open";
 	const str::StringView strSave = "m_Save";
 }
@@ -15,14 +15,14 @@ namespace
 template<>
 void Visitor::ReadCustom(editor::texture::SettingsComponent& value) const
 {
-	Read(strExtract, value.m_Import, value.m_Import);
+	Read(strImport, value.m_Import, value.m_Import);
 	Read(strOpen, value.m_Open, value.m_Open);
 	Read(strSave, value.m_Save, value.m_Save);
 }
 template<>
 void Visitor::WriteCustom(const editor::texture::SettingsComponent& value)
 {
-	Write(strExtract, value.m_Import);
+	Write(strImport, value.m_Import);
 	Write(strOpen, value.m_Open);
 	Write(strSave, value.m_Save);
 }
