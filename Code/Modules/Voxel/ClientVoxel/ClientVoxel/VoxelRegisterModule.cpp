13ad050deed2c0231df9ad96562aef90a2ce1d92
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2026-06-01 19:37:02 -0400

    Update VoxelRegisterModule.cpp

--- Code/Modules/Voxel/ClientVoxel/ClientVoxel/VoxelRegisterModule.cpp
+++ Code/Modules/Voxel/ClientVoxel/ClientVoxel/VoxelRegisterModule.cpp
@@ -4,10 +4,12 @@
 #include "ECS/EntityWorld.h"
 #include "ECS/WorldView.h"
 #include "ClientVoxel/VoxelMeshingSystem.h"
+#include "ClientVoxel/VoxelModifySettingsComponent.h"
 #include "ClientVoxel/VoxelModifySystem.h"
 
 void client::voxel::RegisterModule(ecs::EntityWorld& world)
 {
+	world.RegisterComponent<client::voxel::ModifySettingsComponent>();
 	world.RegisterSystem<client::voxel::MeshingSystem>();
 	world.RegisterSystem<client::voxel::ModifySystem>();
 }
