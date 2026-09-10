2521fb3830ece1ad576f1bf3d4099c348af8cf89
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2026-09-09 22:58:56 -0400

    Update EntityLayout.h

--- Code/Framework/ECS/ECS/EntityLayout.h
+++ Code/Framework/ECS/ECS/EntityLayout.h
@@ -15,16 +15,16 @@ namespace ecs
 		bool operator<(const ecs::EntityLayout& rhs) const
 		{
 			return (m_AddedMask != rhs.m_AddedMask)
-				? (m_AddedMask   < rhs.m_AddedMask)   : (m_RemovedMask != rhs.m_RemovedMask)
-				? (m_RemovedMask < rhs.m_RemovedMask) : (m_IncludeMask != rhs.m_IncludeMask)
-				? (m_IncludeMask < rhs.m_IncludeMask) : (m_IsDead < rhs.m_IsDead);
+				? (m_AddedMask   < rhs.m_AddedMask)   : (m_IncludeMask != rhs.m_IncludeMask)
+				? (m_IncludeMask < rhs.m_IncludeMask) : (m_RemovedMask != rhs.m_RemovedMask)
+				? (m_RemovedMask < rhs.m_RemovedMask) : (m_IsDead < rhs.m_IsDead);
 		}
 
-		ecs::ComponentMask GetMask() const { return m_AddedMask | m_RemovedMask | m_IncludeMask; };
+		ecs::ComponentMask GetMask() const { return m_AddedMask | m_IncludeMask | m_RemovedMask; };
 
 		ecs::ComponentMask m_AddedMask = {};
-		ecs::ComponentMask m_RemovedMask = {};
 		ecs::ComponentMask m_IncludeMask = {};
+		ecs::ComponentMask m_RemovedMask = {};
 		bool m_IsDead = false;
 	};
 }
